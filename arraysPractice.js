ec831fa238322f4d3954ca1ea3a45a91fbcfda60
Brant Bagnall <brant.bagnall@gmail.com>
2017-09-26 16:48:33 -0600

    finished last required problem

--- arraysPractice.js
+++ arraysPractice.js
@@ -42,9 +42,8 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
   //Code Here
   function reversedLooper (letters) {
-    for (var i = 0; i < letters.length; i++) {
-      alert(letters.pop());
-      letters.pop();
+    for (var i = letters.length - 1; i >= 0; i--) {
+      alert(letters[i]);
     }
   }
 
